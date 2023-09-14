606ea3aaadc76e6833428beb19228da3d80fe3e0
gurpreet808 <gurpreets808@gmail.com>
2023-09-14 13:06:36 -0300

    Ruteo inicial funcionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './componentes/login/login.component';
+import { Error404Component } from './componentes/error404/error404.component';
+import { QuienSoyComponent } from './componentes/quien-soy/quien-soy.component';
+import { HomeComponent } from './componentes/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{ path: "", redirectTo: "/bienvenido", pathMatch: "full" },
+  { path: "", component: HomeComponent },
+  { path: "login", component: LoginComponent },
+  //{ path: "registrar-usuario", component: RegistrarUsuarioComponent },
+  { path: "quien-soy", component: QuienSoyComponent },
+  { path: "**", component: Error404Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
