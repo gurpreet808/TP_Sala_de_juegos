b2fe71abc67654184ca04ed9a3a61f8a9a8ffc43
gurpreet808 <gurpreets808@gmail.com>
2023-09-14 12:38:11 -0300

    Agrego angularFire

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'lab4-saladejuegos',
+    appId: '1:1091172030495:web:45a316f2f560a782235d2a',
+    storageBucket: 'lab4-saladejuegos.appspot.com',
+    apiKey: 'AIzaSyB9ijQYN7qGGqeOrLjuNwCaVx1hrUYokcQ',
+    authDomain: 'lab4-saladejuegos.firebaseapp.com',
+    messagingSenderId: '1091172030495',
+  },};
