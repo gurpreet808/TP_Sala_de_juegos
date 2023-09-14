25e45ce695a231c40da424b8d92879ef3380038a
gurpreet808 <gurpreets808@gmail.com>
2023-09-14 15:46:00 -0300

    Agrego datos quien soy

--- src/app/componentes/quien-soy/quien-soy.component.ts
+++ src/app/componentes/quien-soy/quien-soy.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./quien-soy.component.scss']
 })
 export class QuienSoyComponent {
-
+  //url_foto: string = 'assets/img/foto.jpg';
+  url_foto: string = "https://avatars.githubusercontent.com/u/17884026";
+  nombre: string = 'Daniel Singh';
+  descripcion_juego: string = "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 }
