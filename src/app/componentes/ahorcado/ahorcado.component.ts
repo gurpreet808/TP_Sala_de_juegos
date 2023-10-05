faa0bff543e277e95a1819b336bd77af09c1aefa
gurpreet808 <gurpreets808@gmail.com>
2023-10-05 19:51:18 -0300

    Mejora en mensajes y correción de si gano o no

--- src/app/componentes/ahorcado/ahorcado.component.ts
+++ src/app/componentes/ahorcado/ahorcado.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-ahorcado',
@@ -16,7 +17,7 @@ export class AhorcadoComponent {
     'gato', 'perro', 'casa', 'coche',
   ];
 
-  constructor() {
+  constructor(public messageService: MessageService) {
     this.ResetJuego();
   }
 
@@ -27,7 +28,7 @@ export class AhorcadoComponent {
     this.letras = 'abcdefghijklmnñopqrstuvwxyz'.toUpperCase().split('');
   }
 
-  PalabraAlAzar() {
+  async PalabraAlAzar() {
     this.palabra_a_adivinar = this.palabras[Math.floor(Math.random() * this.palabras.length)].toUpperCase();
     console.log(this.palabra_a_adivinar);
 
@@ -41,23 +42,26 @@ export class AhorcadoComponent {
     return false;
   }
 
-  ElegirLetra(letra: string) {
-    console.log(letra, this.ComprobarLetra(letra));
-    
+  async ElegirLetra(letra: string) {
+    //console.log(letra, this.ComprobarLetra(letra));
     this.letras.splice(this.letras.indexOf(letra), 1);
 
     if (this.ComprobarLetra(letra)) {
       this.letras_adivinadas.push(letra);
-      this.ActualizarPalabraOculta();
+      await this.ActualizarPalabraOculta();
+
+      if (this.palabra_a_adivinar.split('').every(letra => this.letras_adivinadas.includes(letra))) {
+        this.Ganaste();
+      }
     } else {
       this.intentos_restantes--;
       if (this.intentos_restantes == 0) {
-        this.finalizado = true;
+        this.Perdiste();
       }
     }
   }
 
-  ActualizarPalabraOculta() {
+  async ActualizarPalabraOculta() {
     for (let x = 0; x < this.palabra_a_adivinar.length; x++) {
       if (this.letras_adivinadas.includes(this.palabra_a_adivinar[x])) {
         this.palabra_oculta = this.palabra_oculta.substr(0, x) + this.palabra_a_adivinar[x] + this.palabra_oculta.substr(x + 1);
@@ -65,12 +69,24 @@ export class AhorcadoComponent {
     }
   }
 
-  ResetJuego() {
+  async ResetJuego() {
     this.intentos_restantes = 5;
     this.letras_adivinadas = [];
     this.palabra_oculta = '';
     this.finalizado = false;
     this.TecladoInit();
-    this.PalabraAlAzar();
+    await this.PalabraAlAzar();
+  }
+
+  Ganaste() {
+    this.finalizado = true;
+    console.log('Ganaste');
+    this.messageService.add({ severity: 'success', summary: 'Ganaste', detail: 'Felicidades, has ganado el juego! :)' });
+  }
+
+  Perdiste() {
+    this.finalizado = true;
+    console.log('Perdiste');
+    this.messageService.add({ severity: 'error', summary: 'Perdiste', detail: 'Has perdido el juego, inténtalo de nuevo! :(' });
   }
 }
