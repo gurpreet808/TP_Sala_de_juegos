bb023bbd124527ea4577816cb0a0ad3e273fc87d
gurpreet808 <gurpreets808@gmail.com>
2023-10-12 20:19:32 -0300

    Actualizo descripcion del juego propio

--- src/app/componentes/quien-soy/quien-soy.component.ts
+++ src/app/componentes/quien-soy/quien-soy.component.ts
@@ -9,5 +9,4 @@ export class QuienSoyComponent {
   //url_foto: string = 'assets/img/foto.jpg';
   url_foto: string = "https://avatars.githubusercontent.com/u/17884026";
   nombre: string = 'Daniel Singh';
-  descripcion_juego: string = "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 }
